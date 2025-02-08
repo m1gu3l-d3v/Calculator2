f43c1edf7abba710b64aff5700f06e9e501c820a
AJ <135352588+AsthriPardave@users.noreply.github.com>
2025-02-08 00:28:20 -0500

    Inicio proyecto

---
+++ src/controlador/ControladorAlgoritmos.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+/**
+ *
+ * @author HP
+ */
+public class ControladorAlgoritmos {
+    
+    public static void infijaAprefija(){
+    
+    }
+    
+    public static void infijaAposfija(){
+    
+    }    
+    
+    public static void resolverPosfija(){
+    
+    }
+    
+    public static void resolverPrefija(){
+    
+    }
+    
+    public static void djikstra(){
+    
+    }
+    
+    public static void huffman(){
+    
+    }
+    
+    public static void ordenar(){
+    
+    }
+    
+    public static void prim(){
+    
+    }
+    
+}
