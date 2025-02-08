c92ed1d4f4b78ba17dc581f5f38774f349e29a76
José Miguel <ppoongtv@yahoo.com>
2025-02-08 16:06:22 -0500

    dsfsdsdfsdf

---
+++ src/estructuras/NodoGenerico.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estructuras;
+
+/**
+ *
+ * @author DALGUMA
+ * @param <Tipo> cualquier tipo pq me pidem parametrear esto ahhhhhhhhhhh
+ */
+public class NodoGenerico<Tipo> {
+    Tipo dato; 
+    NodoGenerico siguiente;
+    NodoGenerico previo;
+    public NodoGenerico(Tipo dato) {
+        this.dato = dato;
+        this.siguiente=null;
+        this.previo=null;
+    }
+}
---
+++ src/estructuras/PilaGenerica.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estructuras;
+
+/**
+ *
+ * @author DALGUMA
+ * @param <Tipo> cualquier tipo pues
+ */
+public class PilaGenerica {
+    NodoGenerico Cima;
+    int tamaño;
+//Constructor
+    public PilaGenerica() {
+        this.Cima = null;
+        tamaño = 0;
+    }
+//
+    
+    
+}
